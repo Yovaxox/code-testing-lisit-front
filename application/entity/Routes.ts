ad4f75992845255ac619a5191fad8e4c5ceffa6c
javier.paredes@outlook.com <javier.paredes@outlook.com>
2024-02-23 05:49:31 -0300

    Update API route

--- application/entity/Routes.ts
+++ application/entity/Routes.ts
@@ -1,8 +1,8 @@
 export class con {
   public static ReturnRoute() {
     let route: string = ''
-    route = 'https://localhost:7030/api/'
-    //route = 'https://thelastbug.azurewebsites.net/api/'
+    //route = 'https://localhost:7030/api/'
+    route = 'https://thelastbug.azurewebsites.net/api/'
 
     return route
   }
